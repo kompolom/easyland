e3866b8f14b42eaf1fd980a86635014ad10b14a7
Evgeniy Baranov <master@kompolom.ru>
2014-10-30 15:54:09 +0300

    add: html заготовка

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  require 'lib/init.php';?>
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <link rel="icon" href="favicon.ico" />
+    <link href="assets/_index.css" rel="stylesheet" type="text/css" />    
+    <link href="assets/style.css" rel="stylesheet" type="text/css" />    
+</head>
+<body class="page">
+ <div class="page__wrapper">
+
+ </div><!--page__wrapper-->
+ <div class="hidden">
+ </div>
+ <script async src="assets/_index.js"></script>
+  <?php HTML::metrika(12345678); ?>
+</body>
+</html>
